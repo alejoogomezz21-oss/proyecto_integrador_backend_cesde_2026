8f07ce09576ac8ae7daf79fa257993a2fd1d20ed
alejogomez21 <alejogoji777@gmail.com>
2026-03-03 12:20:22 -0500

    Creacion del modelo UserRoles

---
+++ src/main/java/co/edu/cesde/ga/aplication/models/UserRoles.java
@@ -0,0 +1,19 @@
+package co.edu.cesde.ga.aplication.models;
+
+public class UserRoles {
+    private Long userId; // user_id
+    private Long roleId; // role_id
+
+    public UserRoles() {}
+
+    public UserRoles(Long userId, Long roleId) {
+        this.userId = userId;
+        this.roleId = roleId;
+    }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public Long getRoleId() { return roleId; }
+    public void setRoleId(Long roleId) { this.roleId = roleId; }
+}
