2b368c530392d9e1b86df24c9fefb10671e74118
alejogomez21 <alejogoji777@gmail.com>
2026-03-03 11:52:51 -0500

    construccion: establecer estructura profesional co.edu.cesde.ga

---
+++ src/main/java/co/edu/cesde/ga/aplication/app/Main.java
@@ -0,0 +1,7 @@
+package co.edu.cesde.ga.aplication.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Proyecto Integrador Iniciado correctamente");
+    }
+}
