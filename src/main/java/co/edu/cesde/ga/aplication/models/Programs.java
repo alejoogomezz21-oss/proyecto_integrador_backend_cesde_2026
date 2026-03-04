8acd71893f7f91cbcf20079c2f74378c04990bc7
mafe correa <qcorreamafe591@gmail.com>
2026-03-04 11:14:23 -0500

    agregar clases de estructura academica

---
+++ src/main/java/co/edu/cesde/ga/aplication/models/Programs.java
@@ -0,0 +1,26 @@
+package co.edu.cesde.ga.aplication.models;
+
+public class Programs {
+
+
+        private Long programId;
+        private String code;
+        private String name;
+
+        public Programs() {}
+
+        public Programs(Long programId, String code, String name) {
+            this.programId = programId;
+            this.code = code;
+            this.name = name;
+        }
+
+
+        public Long getProgramId() { return programId; }
+        public void setProgramId(Long programId) { this.programId = programId; }
+        public String getCode() { return code; }
+        public void setCode(String code) { this.code = code; }
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+    }
+
