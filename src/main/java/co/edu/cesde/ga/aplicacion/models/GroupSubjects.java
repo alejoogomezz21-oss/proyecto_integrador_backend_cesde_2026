b109f236b08f02e3c051007cafe04328a77b8bea
Juan David <juandalopeztrujillo@gmail.com>
2026-05-29 16:06:12 -0500

    trabajo de nayla

--- src/main/java/co/edu/cesde/ga/aplicacion/models/GroupSubjects.java
+++ src/main/java/co/edu/cesde/ga/aplicacion/models/GroupSubjects.java
@@ -1,27 +1,22 @@
 package co.edu.cesde.ga.aplicacion.models;
 
-public class GroupSubjects {
-    private Long id;
+public class GroupsSubjects {
+    private Long groupSubjectId;
     private Long groupId;
     private Long subjectId;
-    private Long teacherId;
 
-    public GroupSubjects() {}
+    public GroupsSubjects() {}
 
-    public GroupSubjects(Long id, Long groupId, Long subjectId, Long teacherId) {
-        this.id = id;
+    public GroupsSubjects(Long groupSubjectId, Long groupId, Long subjectId) {
+        this.groupSubjectId = groupSubjectId;
         this.groupId = groupId;
         this.subjectId = subjectId;
-        this.teacherId = teacherId;
     }
 
-    // Getters y Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getGroupSubjectId() { return groupSubjectId; }
+    public void setGroupSubjectId(Long groupSubjectId) { this.groupSubjectId = groupSubjectId; }
     public Long getGroupId() { return groupId; }
     public void setGroupId(Long groupId) { this.groupId = groupId; }
     public Long getSubjectId() { return subjectId; }
     public void setSubjectId(Long subjectId) { this.subjectId = subjectId; }
-    public Long getTeacherId() { return teacherId; }
-    public void setTeacherId(Long teacherId) { this.teacherId = teacherId; }
 }
