d4091067ff5a8cf01ce94716a3c2cb26594c4a1b
juandalopeztrujillo-code <juandalopeztrujillo@gmail.com>
2026-05-29 15:49:53 -0500

    fleat-service-nayla.

--- src/main/java/co/edu/cesde/ga/aplicacion/models/Users.java
+++ src/main/java/co/edu/cesde/ga/aplicacion/models/Users.java
@@ -2,59 +2,33 @@ package co.edu.cesde.ga.aplicacion.models;
 
 public class Users {
     private Long userId;
-    private String username;
-    private String password;
+    private String identification;
+    private String name;
+    private String lastName;
     private String email;
-    private String status;
+    private String password;
 
     public Users() {}
 
-    public Users(Long userId, String username, String password, String email, String status) {
+    public Users(Long userId, String identification, String name, String lastName, String email, String password) {
         this.userId = userId;
-        this.username = username;
-        this.password = password;
+        this.identification = identification;
+        this.name = name;
+        this.lastName = lastName;
         this.email = email;
-        this.status = status;
-    }
-
-    // --- ESTO ES LO QUE TE FALTA PARA QUITAR EL ROJO ---
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+    public String getIdentification() { return identification; }
+    public void setIdentification(String identification) { this.identification = identification; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
